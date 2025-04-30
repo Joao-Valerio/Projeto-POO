c6c020e8da2874b763dab01801402e1011b3eff4
João Valério Santana <151927936+ohomiebrabo@users.noreply.github.com>
2025-04-29 21:01:15 -0300

    separation method

--- app.py
+++ app.py
@@ -1,7 +1,92 @@
 import models
 from datetime import datetime
 
+def createUser():
+    name = input("Nome: ")
+    cpf = input("Cpf: ")
+    models.User.create(name=name, cpf=cpf)
 
+def selectUser():
+    print("\n--- Lista de Clientes ---")
+    for user in models.User.select():
+        print(f"{user.id} - {user.name} ({user.cpf})")
+
+def deleteUser():
+    userID = input("Informe o ID do cliente a ser excluido: ")
+    user = models.User.get_by_id(userID)  
+    user.delete_instance()
+    print("Cliente excluído com sucesso!")
+
+def updateUser():
+    userID = input("Informe o ID do cliente a ser atualizado: ")
+    if models.User.select().where(models.User.id == userID).exists():
+        user = models.User.get_by_id(userID)
+        newName = input("Novo nome: ")
+        newCpf = input("Novo CPF: ")
+        user.name = newName
+        user.cpf = newCpf
+        user.save()
+        print("Cliente atualizado com sucesso!")
+    else:
+        print("Cliente não encontrado.")
+
+def createProduct():
+    productName = input("Qual o nome do produto: ")
+    price = input("Informe o preço do produto: ")
+    models.Product.create(productName=productName, price=price)
+
+def selectProduct():
+    print("\n--- Lista de Produtos ---")
+    for products in models.Product.select():
+        print(f"{products.id} - {products.productName} - {products.price}")
+
+def deleteProduct():
+    productId = input("Informe o ID do produto a ser excluido: ")
+    product = models.Product.get_by_id(productId)
+    product.delete_instance()
+    print("Produto excluído com sucesso!")
+
+def updateProduct():
+    productId = input("ID do produto a ser atualizado: ")
+    if models.Product.select().where(models.Product.id == productId).exists():
+        product = models.Product.get_by_id(productId)
+        newName = input("Novo nome do produto: ")
+        newPrice = input("Novo preço: ")
+        product.productName = newName
+        product.price = newPrice
+        product.save()
+        print("Produto atualizado com sucesso!")
+    else:
+        print("Produto não encontrado.")
+
+def createOrder():
+    userId = input("Qual o ID do usuário que está fazendo o pedido: ")
+    user = models.User.get_by_id(userId)
+    newOrder = models.Order.create(
+    user=user, orderDate=datetime.now(), totalPrice=0.0)
+    print(f"Pedido {newOrder.id} criado para o cliente {user.name}.")
+    itemsOrder(newOrder)
+
+def selectOrder():
+    print("\n--- Lista de Pedidos ---")
+    for order in models.Order.select(models.User, models.Order).join(models.User).order_by(models.Order.id):
+        print(f"ID: {order.id}, Cliente: {order.user.name} (ID: {order.user.id}), "
+        f"Data: {order.orderDate.strftime('%d/%m/%Y %H:%M:%S')}, Total: R$ {order.totalPrice:.2f}")
+
+def deleteOrder():
+    orderId = input("Qual ID do pedido a ser excluido: ")
+    order = models.Order.get_by_id(orderId)
+    order.delete_instance()
+    print("Pedido excluído com sucesso!")
+
+def updateOrder():
+    orderId = input("ID do pedido para adicionar itens: ")
+    if models.Order.select().where(models.Order.id == orderId).exists():
+        pedido = models.Order.get_by_id(orderId)
+        itemsOrder(pedido)
+    else:
+        print("Pedido não encontrado.")
+        
 def userMenu():
     while (True):
         print("\n--- Menu Clientes ---")
@@ -13,30 +98,13 @@ def userMenu():
         op = input("Escolha: ")
 
         if op == "1":
-            name = input("Nome: ")
-            cpf = input("Cpf: ")
-            models.User.create(name=name, cpf=cpf)
+            createUser()
         elif op == "2":
-            print("\n--- Lista de Clientes ---")
-            for user in models.User.select():
-                print(f"{user.id} - {user.name} ({user.cpf})")
+            selectUser()
         elif op == "3":
-            userID = input("Informe o ID do cliente a ser excluido: ")
-            user = models.User.get_by_id(userID)  
-            user.delete_instance()
-            print("Cliente excluído com sucesso!")
+            deleteUser()
         elif op == "4":
-            userID = input("Informe o ID do cliente a ser atualizado: ")
-            if models.User.select().where(models.User.id == userID).exists():
-                user = models.User.get_by_id(userID)
-                newName = input("Novo nome: ")
-                newCpf = input("Novo CPF: ")
-                user.name = newName
-                user.cpf = newCpf
-                user.save()
-                print("Cliente atualizado com sucesso!")
-            else:
-                print("Cliente não encontrado.")
+            updateUser()
         elif op == "0":
             break
         else:
@@ -54,30 +122,13 @@ def productsMenu():
         op = input("Escolha: ")
 
         if op == "1":
-            productName = input("Qual o nome do produto: ")
-            price = input("Informe o preço do produto: ")
-            models.Product.create(productName=productName, price=price)
+            createProduct()
         elif op == "2":
-            print("\n--- Lista de Produtos ---")
-            for products in models.Product.select():
-                print(f"{products.id} - {products.productName} - {products.price}")
+            selectProduct()
         elif op == "3":
-            productId = input("Informe o ID do produto a ser excluido: ")
-            product = models.Product.get_by_id(productId)
-            product.delete_instance()
-            print("Produto excluído com sucesso!")
+            deleteProduct()
         elif op == "4":
-            productId = input("ID do produto a ser atualizado: ")
-            if models.Product.select().where(models.Product.id == productId).exists():
-                product = models.Product.get_by_id(productId)
-                newName = input("Novo nome do produto: ")
-                newPrice = input("Novo preço: ")
-                product.productName = newName
-                product.price = newPrice
-                product.save()
-                print("Produto atualizado com sucesso!")
-            else:
-                print("Produto não encontrado.")
+            updateProduct()
         elif op == "0":
             break
         else:
@@ -95,32 +146,13 @@ def ordersMenu():
         op = input("Escolha: ")
 
         if op == "1":
-            userId = input("Qual o ID do usuário que está fazendo o pedido: ")
-            user = models.User.get_by_id(userId)
-            newOrder = models.Order.create(
-                user=user, orderDate=datetime.now(), totalPrice=0.0)
-            print(f"Pedido {newOrder.id} criado para o cliente {user.name}.")
-            itemsOrder(newOrder)
-
+            createOrder()
         elif op == "2":
-            print("\n--- Lista de Pedidos ---")
-            for order in models.Order.select(models.User, models.Order).join(models.User).order_by(models.Order.id):
-                print(f"ID: {order.id}, Cliente: {order.user.name} (ID: {order.user.id}), "
-                f"Data: {order.orderDate.strftime('%d/%m/%Y %H:%M:%S')}, Total: R$ {order.totalPrice:.2f}")
-
+            selectOrder()
         elif op == "3":
-            orderId = input("Qual ID do pedido a ser excluido: ")
-            order = models.Order.get_by_id(orderId)
-            order.delete_instance()
-            print("Pedido excluído com sucesso!")
+            deleteOrder()
         elif op == "4":
-            orderId = input("ID do pedido para adicionar itens: ")
-            if models.Order.select().where(models.Order.id == orderId).exists():
-                pedido = models.Order.get_by_id(orderId)
-                itemsOrder(pedido)
-            else:
-                print("Pedido não encontrado.")
-        
+            updateOrder()
         elif op == "0":
             break
 

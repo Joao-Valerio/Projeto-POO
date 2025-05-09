9ce8b3af658874d351fc968710595a8240dff297
João Valério Santana <151927936+ohomiebrabo@users.noreply.github.com>
2025-05-08 22:46:00 -0300

    create users module

---
+++ user.py
@@ -0,0 +1,32 @@
+import models
+from datetime import datetime
+
+def createUser():
+    name = input("Nome: ")
+    cpf = input("Cpf: ")
+    models.User.create(name=name, cpf=cpf)
+    print("\n Cliente criado com sucesso!")
+
+def selectUser():
+    print("\n--- Lista de Clientes ---")
+    for user in models.User.select():
+        print(f"{user.id} - {user.name} ({user.cpf})")
+
+def deleteUser():
+    userID = input("Informe o ID do cliente a ser excluido: ")
+    user = models.User.get_by_id(userID)  
+    user.delete_instance()
+    print("Cliente excluído com sucesso!")
+
+def updateUser():
+    userID = input("Informe o ID do cliente a ser atualizado: ")
+    if models.User.select().where(models.User.id == userID).exists():
+        user = models.User.get_by_id(userID)
+        newName = input("Novo nome: ")
+        newCpf = input("Novo CPF: ")
+        user.name = newName
+        user.cpf = newCpf
+        user.save()
+        print("Cliente atualizado com sucesso!")
+    else:
+        print("Cliente não encontrado.")
